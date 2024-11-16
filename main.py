773312020c57ce3767435fb4507b0a01edc015b2
Relya <mohvardeno@gmail.com>
2024-11-16 14:59:41 +0000

    main

--- main.py
+++ main.py
@@ -1,59 +1,56 @@
 import os
-import subprocess
 import sys
-from os import getcwd, getlogin, chdir, system
-from colorama import Fore, Style, init
+from os import getcwd, getlogin, chdir
 from time import sleep
 import readline
+
+# Инициализация colorama
+
+
 try:
     # Получаем имя текущего пользователя
     user_home = os.path.expanduser("~")  # Получаем домашнюю директорию пользователя
     history_file = os.path.join(user_home, '.history')  # Формируем полный путь к файлу истории
 
     # Загружаем историю команд, если файл существует
-    if not os.path.isfile(history_file):  # Проверяем, является ли это файлом
-        history = []  # Если файл не найден, инициализируем пустой список
-    else:
+    if os.path.isfile(history_file):  # Проверяем, является ли это файлом
         with open(history_file, 'r') as f:
             history = f.read().splitlines()
+    else:
+        history = []  # Если файл не найден, инициализируем пустой список
 
     # Добавляем историю команд для автозаполнения
     for cmd in history:
         readline.add_history(cmd)
 
-    init(autoreset=True)
-    sys.stdout.write('PyTerm 2024\n')
-    sys.stdout.flush()
+    print('PyTerm 2024')
 
     while True:
         path = getcwd()
         name = getlogin()
-        command = input(f'{Fore.CYAN}{name}{Style.RESET_ALL} ► {Fore.GREEN}{path}{Style.RESET_ALL} > ')
+        
+        # Формируем строку запроса без цветовых кодов
+        command = input(f'{name} ► {path} > ')
 
         if command.startswith('cd '):
             try:
                 chdir(command[3:])
             except FileNotFoundError:
-                sys.stdout.write(f"cd: no such file or directory: {command[3:]}\n")
+                print(f"cd: no such file or directory: {command[3:]}")
             except PermissionError:
-                sys.stdout.write(f"cd: permission denied: {command[3:]}\n")
+                print(f"cd: permission denied: {command[3:]}")
             continue
 
-        elif command.lower() in ['exit', 'quit']:
-            sys.stdout.write("Выход из PyTerm.\n")
-            sleep(0.5)
-            break
-
         try:
-            system(command)
+            os.system(command)
         except Exception as e:
             error_message = str(e)
             cleaned_error = error_message.replace('/bin/sh: строка 1: ', '')
-            sys.stdout.write(f"{cleaned_error}\n")
+            print(f"{cleaned_error}")
 
         # Сохраняем команду в файл истории
-
         with open(history_file, 'a') as f:
             f.write(command + '\n')
-except:
+
+except Exception as e:
     print('Error!')
